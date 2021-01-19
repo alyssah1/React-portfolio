1556d60635495d15b8f998cbc5c590250beeddae
Alyssa Hollowell <alyssa.hollowell2@gmail.com>
2021-01-18 18:54:43 -0600

    added number and location to contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,51 +4,33 @@ import "../../assets/css/style.css";
 function Contact () {
     return (
     <>
-    <div className="container">
-        <section className="contact-section">
+<div className="container">
+    <section className="contact-section">
         <h1 id="contact">Contact</h1>
+        <h3 className="number"><i className="fas fa-phone-square"></i>(469)-323-1038</h3>
+        <h3 className="location"><i className="fas fa-map-marker-alt"></i>Houston, Texas</h3>
         <hr className="line"></hr>
         <div className="mb-3">
-        <label for="exampleFormControlInput1" className="form-label">First Name</label>
-
+            <label for="exampleFormControlInput1" className="form-label">First Name</label>
+            <input type="firstname" className="form-control" id="exampleFormControlInput1" placeholder=" First Name"></input>
         </div>
-
-
-
-
-
-
-
-
-
-
-        </section>
-
-
-
-
-
-
-
-
-
-    </div>
-
-
+        <div className="mb-3">
+            <label for="exampleFormControlInput1" className="form-label">Last Name</label>
+            <input type="lastname" className="form-control" id="exampleFormControlInput1" placeholder=" Last Name"></input>
+        </div>
+        <div className="mb-3">
+            <label for="exampleFormControlInput1" className="form-label">Email Address</label>
+            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Name@example.com"></input>
+        </div>
+        <div className="mb-3">
+            <label for="exampleFormControlTextarea1" className="form-label">Message</label>
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+        <button type="button" className="btn btn-light btn-lg" style={{backgroundColor: "#6897bb"}}>Submit</button>
+    </section>
+</div>
     </>
-
-
-
-
-
     )
-
-
-
-
-
-
-
 }
 
 
